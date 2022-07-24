c90c9698314c5ba2e436cba6e53b9f57dbb1d77b
Albert Pichler <albertpichler@gmx.net>
2022-07-24 13:48:30 +0200

    Update yml

--- util/playwright/global-setup.ts
+++ util/playwright/global-setup.ts
@@ -7,7 +7,7 @@ const password = process.env.TEST_PASSWORD ?? '';
 async function globalSetup(config: FullConfig): Promise<void> {
   const { storageState } = config.projects[0].use;
   // TODO: Remove headless after this has been tested
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({headless: true});
   const page = await browser.newPage();
   await login(page, username, password);
   await page.context().storageState({
