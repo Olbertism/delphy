a14bf45a2b40487ae7793e125ed64be2df79ab47
Albert Pichler <albertpichler@gmx.net>
2022-07-24 15:23:43 +0200

    Prettier

--- jest.config.js
+++ jest.config.js
@@ -12,17 +12,3 @@ const customJestConfig = {
 };
 
 module.exports = createJestConfig(customJestConfig);
-
-
-
-
-// /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-/* module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  setupFilesAfterEnv: ['./jest.setup.js'],
-}; */
